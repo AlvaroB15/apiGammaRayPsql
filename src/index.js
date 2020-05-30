67d12b16134e82dd204fcfa06367e794269e37f6
Alvaro Begazo <alvaro15unmsm@gmail.com>
2020-05-30 16:06:11 -0500

    2do intento de arreglar el cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded( { extended : false } ) );
 // definicion de 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://gamma-angular.web.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
