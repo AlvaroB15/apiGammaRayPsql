51d63c178d67df3e031b4ecf3b62c64ee7294924
AlvaroB15 <32596011+AlvaroB15@users.noreply.github.com>
2020-08-17 20:48:28 -0500

    intento de cors global

--- src/index.js
+++ src/index.js
@@ -23,12 +23,20 @@ app.use(function(req, res, next) {
     
     // res.header("Access-Control-Allow-Origin", "http://localhost:4200/album");
     
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    //res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     // res.header("Access-Control-Allow-Origin", "https://demoqr-9aed0.web.app/generateQr");
     // res.header("Access-Control-Allow-Origin", "https://demoqr-9aed0.web.app")
-
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
+    //Forma general para que cualquier dominio la consuma, sea local o desplegada
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
     next();
+    
+
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //next();
 });
 
 // app.get('/', function(req, res, next) {
